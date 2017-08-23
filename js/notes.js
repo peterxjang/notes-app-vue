0c005379462f3de0b9de896d604e502a764660e9
Peter Jang <peter.x.jang@gmail.com>
2017-08-23 14:25:18 -0500

    Show selected note on click in left navigation

--- js/notes.js
+++ js/notes.js
@@ -2,14 +2,21 @@
 var app = new Vue({
   el: '#app',
   data: {
-    notes: [
+    notes: [],
+    selectedNote: {}
+  },
+  mounted: function() {
+    this.notes = [
       {id: 1, body: "This is a first test", timestamp: Date.now()},
       {id: 2, body: "This is a second test", timestamp: Date.now()},
       {id: 3, body: "This is a third test", timestamp: Date.now()}
-    ]
+    ];
+    this.selectedNote = this.notes[0];
   },
   methods: {
-
+    selectNote: function(note) {
+      this.selectedNote = note;
+    }
   },
   computed: {
     transformedNotes: function() {
